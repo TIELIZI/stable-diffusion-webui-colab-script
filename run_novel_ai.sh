ce3986c06bdc255006d2962e23c991f6c4182d48
TIELIZI <99740220+TIELIZI@users.noreply.github.com>
2022-10-12 21:11:23 +0800

    Update run_novel_ai.sh

--- run_novel_ai.sh
+++ run_novel_ai.sh
@@ -1,76 +1,79 @@
-#!/bin/bash
-# install pythn3.10
-sudo apt-get install python3.10
-/usr/bin/python3.10 --version
-sudo apt-get install python3.10-distutils && wget https://bootstrap.pypa.io/get-pip.py && /usr/bin/python3.10 get-pip.py
-/usr/bin/python3.10 -m pip install -U pip
+from google.colab import drive
+drive.mount('/content/drive')
 
-# Set defaults
-# Install directory without trailing slash
-install_dir="/content/webui"
-mkdir -p $install_dir
+!python -m pip install --upgrade pip setuptools wheel
+!python -m pip install lbry-libtorrent
+!wget https://raw.githubusercontent.com/TIELIZI/stable-diffusion-webui-colab-script/main/novelaileak.torrent
+import shutil
+import libtorrent as lt
+import time
+import datetime
 
-# Name of the subdirectory (defaults to stable-diffusion-webui)
-clone_dir="stable-diffusion-webui"
+ses = lt.session()
+ses.listen_on(6881, 6891)
 
-# python3 executable
-python_cmd="/usr/bin/python3.10"
+e = lt.bdecode(open("novelaileak.torrent", 'rb').read())
+info = lt.torrent_info(e)
+handle = ses.add_torrent(info, '/content/model/')
+ses.start_dht()
 
-# git executable
-export GIT="git"
+begin = time.time()
+print(datetime.datetime.now())
+print("Starting", handle.name())
 
-# launch script
-LAUNCH_SCRIPT="launch.py"
+file_to_download=[
+  "novelaileak/stableckpt/animevae.pt",
+  "novelaileak/stableckpt/animefull-final-pruned/model.ckpt",
+  "novelaileak/stableckpt/modules/modules/aini.pt",
+  "novelaileak/stableckpt/modules/modules/anime.pt",
+  "novelaileak/stableckpt/modules/modules/anime_2.pt",
+  "novelaileak/stableckpt/modules/modules/anime_3.pt",
+  "novelaileak/stableckpt/modules/modules/furry.pt",
+  "novelaileak/stableckpt/modules/modules/furry_2.pt",
+  "novelaileak/stableckpt/modules/modules/furry_3.pt",
+  "novelaileak/stableckpt/modules/modules/furry_kemono.pt",
+  "novelaileak/stableckpt/modules/modules/furry_protogen.pt",
+  "novelaileak/stableckpt/modules/modules/furry_scalie.pt",
+  "novelaileak/stableckpt/modules/modules/furry_transformation.pt",
+  "novelaileak/stableckpt/modules/modules/pony.pt"
+]
+info = handle.get_torrent_info()
 
-# Disable sentry logging
-export ERROR_REPORTING=FALSE
+for i in range(info.num_pieces()):
+  handle.piece_priority(i,0)
+file_index = 0
+for file in info.files():
+    if info.files().file_path(file_index) in file_to_download:
+      print(info.files().file_path(file_index))
+      pr = info.map_file(file_index, 0, 0)
+      pr_next = info.map_file(file_index+1, 0, 0)
+      for i in range(info.num_pieces()):
+        if i in range(pr.piece, pr_next.piece+1):
+          handle.piece_priority(i,7)
+          
+    file_index += 1
 
-# Do not reinstall existing pip packages on Debian/Ubuntu
-export PIP_IGNORE_INSTALLED=0
+while (not handle.is_seed()):
+    s = handle.status()
+    state_str = ['queued', 'checking', 'downloading metadata', \
+    'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
+    print('%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+            (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
+            s.num_peers, state_str[s.state]))
+    if s.progress>=1:
+        break
+    time.sleep(1)
 
-# Pretty print
-delimiter="################################################################"
+end = time.time()
+print(handle.name(), "COMPLETE")
 
-printf "\n%s\n" "${delimiter}"
-printf "\e[1m\e[32mInstall script for stable-diffusion + Web UI\n"
-printf "\e[1m\e[34mTested on Debian 11 (Bullseye)\e[0m"
-printf "\n%s\n" "${delimiter}"
+print("Elapsed Time: ",int((end-begin)//60),"min :", int((end-begin)%60), "sec")
 
-if [ -d .git ]
-then
-    printf "\n%s\n" "${delimiter}"
-    printf "Repo already cloned, using it as install directory"
-    printf "\n%s\n" "${delimiter}"
-    install_dir="${PWD}/../"
-    clone_dir="${PWD##*/}"
-fi
+print(datetime.datetime.now())
 
-printf "\n%s\n" "${delimiter}"
-printf "Clone or update stable-diffusion-webui"
-printf "\n%s\n" "${delimiter}"
-cd "${install_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/, aborting...\e[0m" "${install_dir}"; exit 1; }
-if [ -d "${clone_dir}" ]
-then
-    cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
-    "${GIT}" pull
-else
-    "${GIT}" clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git "${clone_dir}"
-    cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
-fi
-
-# download moel
-# filename="model.zip"
-# curl -Lb ./cookie "https://www.dropbox.com/s/kf6cp7g97ooe1jf/models.zip?dl=0" -o ${filename}
-# unzip -o model.zip -d /content/webui/stable-diffusion-webui/
-if [ -d "/content/models/" ]
-then
-    mkdir -p /content/webui/stable-diffusion-webui/models/Stable-diffusion/
-    mkdir -p /content/webui/stable-diffusion-webui/models/hypernetworks/
-    mv /content/models/Stable-diffusion/* /content/webui/stable-diffusion-webui/models/Stable-diffusion/
-    mv /content/models/hypernetworks/* /content/webui/stable-diffusion-webui/models/hypernetworks/
-fi
-
-printf "\n%s\n" "${delimiter}"
-printf "Launching launch.py..."
-printf "\n%s\n" "${delimiter}"
-"${python_cmd}" "${LAUNCH_SCRIPT}"  --share
+print("Moving model to Google Drive...")
+!mkdir -p /content/drive/MyDrive/models/Stable-diffusion/
+!mv /content/model/novelaileak/stableckpt/animevae.pt /content/drive/MyDrive/models/Stable-diffusion/final-pruned.vae.pt 
+!mv /content/model/novelaileak/stableckpt/animefull-final-pruned/model.ckpt /content/drive/MyDrive/models/Stable-diffusion/final-pruned.ckpt
+!mv /content/model/novelaileak/stableckpt/modules/modules/ /content/drive/MyDrive/models/hypernetworks
+print("Success!")
